aac1130da4f56f330e41ab31d68865c57f35068f
vitalis-virtus <vshayuk@gmail.com>
2022-09-27 15:08:05 +0300

    write handle functions for form and for hello

--- main.go
+++ main.go
@@ -8,6 +8,34 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// checking the correctness of path
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+	}
+
+	//checking the method of request
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	// checking for error when parse form
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
+	}
+	fmt.Fprintf(w, "POST request successful")
+
+	//getting values from form
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprint(w, "Name: %s,\nAddress: %s\n", name, address)
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
