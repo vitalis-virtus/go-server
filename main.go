73b02c1d782b17ad0a08efbf47d01a43522395bb
vitalis-virtus <vshayuk@gmail.com>
2022-09-27 15:00:57 +0300

    configured server in main.go

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+// static web server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server on port 8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
