b83f5bec681a65940866fd74e8aee179cab57e00
vitalis-virtus <vshayuk@gmail.com>
2022-09-27 16:03:04 +0300

    fix form output

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 )
 
+// hello handle function
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// checking the correctness of path
 	if r.URL.Path != "/hello" {
@@ -22,18 +23,20 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+// form handle function
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	// checking for error when parse form
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 
 	//getting values from form
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 
-	fmt.Fprint(w, "Name: %s,\nAddress: %s\n", name, address)
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s \n", address)
 }
 
 func main() {
